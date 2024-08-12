9caac4e8edc01556482be33bc6e11b9d4e8f7d15
Marcelo Fernando <marceloffernando8@gmail.com>
2024-08-12 17:30:08 -0300

    new script

---
+++ script.js
@@ -0,0 +1,7 @@
+function login(event){
+    event.preventDefault();
+    const nome = document.querySelector('#inome').value
+    const sobrenome = document.querySelector('#isobre').value
+    const res = document.querySelector('#res')
+    res.innerHTML = `ola ${nome} ${sobrenome}`
+}
